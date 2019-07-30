06a2813a34229c56f5c8c102f5267f4e13b81fd8
armine <643360052@qq.com>
2019-07-30 09:05:36 +0800

    添加redux-devTools调试工具

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducer';
 import thunk from 'redux-thunk';
+import { createStore,applyMiddleware , compose} from 'redux';
 
-export const store = createStore(reducer,applyMiddleware(thunk));
+// 使用 redux-devTools 调试工具：
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+export const store = createStore(reducer, /* preloadedState, */ composeEnhancers(applyMiddleware(thunk)))
